f7c73c3e91ac83fe81ec7bf90f76a45eb894e681
a-maged <maged.dev@gmail.com>
2021-08-20 00:24:08 +0200

    updated example

--- example/index.tsx
+++ example/index.tsx
@@ -1,20 +1,21 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { useCalendar } from '../.';
-import { format, getDate, getMonth } from 'date-fns';
+import { useCalendar, CalendarProvider, useCalendarContext } from '../src';
+import { format, getDate } from 'date-fns';
 
 const App = () => {
+  const calendar = useCalendar();
+
   const {
     calendarMatrix,
     nextWeek,
     nextCalendar,
     previousWeek,
     previousCalendar,
-    currentWeek,
     goToToday,
     targetDate,
-  } = useCalendar();
+  } = calendar;
 
   return (
     <div>
@@ -39,12 +40,30 @@ const App = () => {
         </>
       ))}
 
-      <h1>Week</h1>
-      {currentWeek.map(day => (
-        <span>{getDate(day)} | </span>
-      ))}
+      <CalendarProvider {...calendar} events={[]}>
+        <WeekExample />
+      </CalendarProvider>
     </div>
   );
 };
+
+type MyCustomEvent = {
+  summary: string;
+  description: string;
+};
+
+function WeekExample() {
+  const { currentWeek, events } = useCalendarContext<MyCustomEvent>();
+
+  return (
+    <>
+      <h1>Week</h1>
+
+      {currentWeek.map(day => (
+        <span>{getDate(day)} | </span>
+      ))}
+    </>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
